8b8d8d18f7318e50f430427a798ae9734f6c5dd2
SolomonVP <Lum7023@yandex.ru>
2024-05-15 02:50:44 +0300

    add POJO

---
+++ app/src/main/java/r/d/cryptoapp/pojo/CoinInfoListOfData.kt
@@ -0,0 +1,12 @@
+package r.d.cryptoapp.pojo
+
+import com.google.gson.annotations.Expose
+
+import com.google.gson.annotations.SerializedName
+
+
+class CoinInfoListOfData (
+    @SerializedName("Data")
+    @Expose
+    private val data: List<Datum>? = null
+)
