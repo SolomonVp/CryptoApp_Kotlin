15673c702bb8d20b300e0f76912b0d42799e0a1f
SolomonVP <Lum7023@yandex.ru>
2024-05-18 01:43:46 +0300

    + service api

--- app/src/main/java/r/d/cryptoapp/MainActivity.kt
+++ app/src/main/java/r/d/cryptoapp/MainActivity.kt
@@ -2,10 +2,35 @@ package r.d.cryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+
+import r.d.cryptoapp.api.ApiFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val disposable = ApiFactory.apiService.getFullPriceList(fSyms =  "BTC")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TEST_OF_LOADING_DATA", it.toString())
+            }, {
+                it.message?.let { it1 -> Log.d("TEST_OF_LOADING_DATA", it1) }
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
---
+++ app/src/main/java/r/d/cryptoapp/database/AppDatabase.kt
@@ -0,0 +1,4 @@
+package r.d.cryptoapp.database
+
+class AppDatabase {
+}
